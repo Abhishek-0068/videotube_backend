c5c37844a9e1939d9fe8f97ff0f3f5ecebbe1c0d
Abhishek-0068 <alpha20abhi@gmail.com>
2024-03-24 07:45:06 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -23,5 +23,15 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 
+//routes import
+import userRouter from './routes/user.routes.js'
+
+//routes declaration
+app.use("/api/v1/users",userRouter)
+
+//http://localhost:8000/users/register
+//http://localhost:8000/users/login
+//http://localhost:8000/api/v1/users/register
+
 
 export { app };
